5476550b49ba33f7062655e23696c25cfa9919a3
sudarshan <sudarshan@mysite.com>
2019-12-17 11:58:48 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite_1/wsgi.py
+++ mysite_1/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite_1.settings')
-
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
